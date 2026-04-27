3f29bc1066ee02495b760c8f43d1511681d3f7a9
anistra <anistratnastya@gmail.com>
2026-04-27 20:26:50 +0300

    Пересмотрела логику работы. Добавила проверку на пустоту. Расширила класс, добавила несколько новых функций

--- src/main/java/com/example/Container.java
+++ src/main/java/com/example/Container.java
@@ -1,11 +1,15 @@
 package com.example;
 
 public class Container {
-    private static class Node {
+    static class Node {
         private int data;
         private Node next;
     }
     private Node head;
+
+    public boolean isEmpty() {
+        return head == null;
+    }
 
     public void add(int x) {
         Node node = new Node();
@@ -14,57 +18,84 @@ public class Container {
         head = node;
     }
 
-    public Node getByValue(int x) {
-        Node node = head;
-        while (node != null) {
-            if (node.data == x)
-                return node;
-            node = node.next;
-        }
-        return null;
-    }
-
-    public Node getInOrder(int n) {
+    public Integer getInOrder(int n) {
         if (n <= 0) return null;
+        if (isEmpty()) return null;
+        
         Node node = head;
         for (int i=1;i<n;++i) {
-            if (node != null) {
-                node = node.next;
-            }
-            else return null;
+            if (node.next == null)
+                return null;
+            node = node.next;
         }
-        return node;
+        return node.data;
     }
 
-    public void deleteByValue(int x) {
-        if (head.data == x)
-            head = head.next;
+    public void deleteFirstByValue(int x) {
+        if (!isEmpty()) {
+            if (head.data == x)
+                head = head.next;
 
-        Node node = head;
-        boolean flag = false;
-        while (node.next != null && !flag) {
-            if (node.next.data == x) {
-                node.next = node.next.next;
-                flag = true;
+            Node node = head;
+            boolean flag = false;
+            while (node.next != null && !flag) {
+                if (node.next.data == x) {
+                    node.next = node.next.next;
+                    flag = true;
+                }
+                node = node.next;
             }
-            node = node.next;
+        }
+    }
+
+    public void deleteLastByValue(int x) {
+        if (!isEmpty()) {
+            Node node = head;
+            Node delNode = null;
+            while (node.next != null) {
+                if (node.next.data == x)
+                    delNode = node;
+                node = node.next;
+            }
+            if (delNode != null)
+                delNode.next = delNode.next.next;
+        }
+    }
+
+    public void deleteAllByValue(int x) {
+        if (!isEmpty()) {
+            while (head != null && head.data == x)
+                head = head.next;
+
+            Node node = head;
+            while (node.next != null) {
+                if (node.next.data == x)
+                    node.next = node.next.next;
+                node = node.next;
+            }
         }
     }
 
     public void deleteInOrder(int n) {
-        if (n>0) {
-            if (n == 1)
-                head = head.next;
-            else {
-                Node node = head;
-                for (int i=1;i<n-1;++i) {
-                    if (node.next != null)
-                        node = node.next;
-                    else node = null;
+        if (!isEmpty()) {
+            if (n>0) {
+                if (n == 1)
+                    head = head.next;
+                else {
+                    Node node = head;
+                    for (int i=1;i<n-1;++i) {
+                        if (node.next != null)
+                            node = node.next;
+                        else node = null;
+                    }
+                    if (node != null)
+                        node.next = node.next.next;
                 }
-                if (node != null)
-                    node.next = node.next.next;
             }
         }
+    }
+
+    public void clear() {
+        head = null;
     }
 }
